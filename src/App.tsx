2a245af83f0425efb2f0e81c0c8efc339ebb6bb4
José Nadin <josenadin01@gmail.com>
2022-06-24 21:04:31 -0300

    Build: Incorporating Apollo and GraphQL to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
+import { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 function App() {
+  const { data }= useQuery<{ lessons: Lesson[]}>(GET_LESSONS_QUERY) //No TS posso tanto tipar aqui dizendo que o retorno da query vai ser um array de lessons
+                                                      // como não é só um array e sim um objeto contendo o array de lessons ficou assim.
   return (
-    <h1 className="text-5xl bold text-violet-600">Oi</h1>
+    <ul>
+      {data?.lessons.map(lesson => { // ou dessa forma: {data.lessons.map((lesson: Lesson)  => {
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
   )
 }
 
