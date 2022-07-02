40d52c5a83f28c71e444fff80af1258abe0e4ff7
José Nadin <93929513+josenadin01@users.noreply.github.com>
2022-07-02 18:00:04 -0300

    Test: testing vercel automated deployment

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -72,7 +72,7 @@ export function Video(props: VideoProps) {
           <div className="flex flex-col gap-4">
             <a
               href=""
-              className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors"
+              className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors pointer-events-none"
             >
               <DiscordLogo size={24} />
               Comunidade do Discord
@@ -80,7 +80,7 @@ export function Video(props: VideoProps) {
 
             <a
               href=""
-              className="p-4 text-sm border border-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors"
+              className="p-4 text-sm border border-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors pointer-events-none"
             >
               <Lightning size={24} />
               Acesse o desafio
@@ -91,7 +91,7 @@ export function Video(props: VideoProps) {
         <div className="gap-8 mt-20 grid grid-cols-2">
           <a
             href=""
-            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors pointer-events-none"
           >
             <div className="bg-green-700 h-full p-6 flex items-center">
               <FileArrowDown size={40} />
@@ -110,7 +110,7 @@ export function Video(props: VideoProps) {
 
           <a
             href=""
-            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors"
+            className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors pointer-events-none"
           >
             <div className="bg-green-700 h-full p-6 flex items-center">
               <FileArrowDown size={40} />
